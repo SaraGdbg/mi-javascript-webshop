16da4f14f133095d3c9444007588e8398d39213c
Sara Gdbg <146811766+SaraGdbg@users.noreply.github.com>
2023-11-18 22:08:31 +0100

    :sparkles: Startat att loopa produkter

--- js/script.js
+++ js/script.js
@@ -2,105 +2,224 @@ console.log("this works!");
 /**
  *  TEKOPPAR
  */
+
 const teacups = [
   {
     name: "kristall",
     price: 249,
     category: "med öra",
     rating: 3,
+    amount: 0,
+    img: {
+      src: "assets/imgs/1KRISTALLpexels-ahmed-aqtai-2013749.jpg",
+      alt: "Genomskinlig kopp av glas med fasat mönster längst ner, fylld med te.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "havsbris",
     price: 349,
     category: "med öra",
     rating: 5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/2HAVSBRISpexels-alteredsnaps-11795940.jpg",
+      alt: "En blågrön handdjord keramikmugg fylld med vit dryck",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "retro",
     price: 179,
     category: "med öra",
     rating: 4.5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/3RETROpexels-ann-buht-762092.jpg",
+      alt: "En röd emajlkopp med stora vita prickar som har ånga som stiger upp ur den.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "flamma",
     price: 179,
     category: "med fat",
     rating: 3,
+    amount: 0,
+    img: {
+      src: "assets/imgs/4FLAMMApexels-archie-binamira-1187317.jpg",
+      alt: "En röd högblank kopp på ett rött högblankt fat.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "surprise",
     price: 179,
     category: "med öra",
     rating: 3,
+    amount: 0,
+    img: {
+      src: "assets/imgs/5SURPRISEpexels-dagmara-dombrovska-18509005.jpg",
+      alt: "En vit kopp med guldkant och texten Let life surprise you tryckt i guld.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "mjau",
     price: 179,
     category: "med öra",
     rating: 5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/6MJAUpexels-dagmara-dombrovska-18620397.jpg",
+      alt: "En vit kopp med ett kattansikte tryckt på framsidan. Koppens öra är en kattsvans.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "mormor",
     price: 195,
     category: "med fat",
     rating: 3,
+    amount: 0,
+    img: {
+      src: "assets/imgs/7MORMORpexels-gülşah-aydoğan-19040177.jpg",
+      alt: "En gammaldags vit kopp med guldkant på ett fat med guldkant. Koppen är fylld med ett rött te.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "sandstrand",
     price: 349,
     category: "utan öra",
     rating: 4.5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/8SANDSTRANDpexels-john-diez-7578231.jpg",
+      alt: "En handgjord sandfärgad kopp utan öra. Snöret till en tepåse hänger ut över kanten.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "glasklar",
     price: 129,
     category: "med öra",
     rating: 4,
+    amount: 0,
+    img: {
+      src: "assets/imgs/9GLASKLARpexels-julia-sakelli-905485 (1).jpg",
+      alt: "En mindre kopp i klart glas, fylld med rykande te.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "fiskben",
     price: 349,
     category: "med öra",
     rating: 4.5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/10FISKBENpexels-karolina-grabowska-4466255.jpg",
+      alt: "En handgjord kopp med ett vertikalt fiskbensmönster i beige och vitt.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "naturlig",
     price: 99,
     category: "utan öra",
     rating: 5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/11NATURLIGpexels-vie-studio-8148643.jpg",
+      alt: "En brun pappersmugg utan lock.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "hjärtat",
     price: 179,
     category: "med öra",
     rating: 4,
+    amount: 0,
+    img: {
+      src: "assets/imgs/12HJÄRTATpexels-lisa-fotios-228184.jpg",
+      alt: "En vaniljgul kopp med en brun kontur av ett stort hjärta på framsidan.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "stilren",
     price: 249,
     category: "med öra",
     rating: 3.5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/13STILRENpexels-polina-kovaleva-8101157.jpg",
+      alt: "En vit slät och högblank kopp utan tryck eller annat.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "berså",
     price: 249,
     category: "med fat",
     rating: 4,
+    amount: 0,
+    img: {
+      src: "assets/imgs/14BERSÅpexels-sena-10240647.jpg/",
+      alt: "Ljusgrön mindre kopp med vitt blommönster på tillhörande fat. I koppen flyter en citronskiva i te.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "namaste",
     price: 299,
     category: "med öra",
     rating: 5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/15NAMASTEpexels-teona-swift-6913386.jpg",
+      alt: "Ljusgrå kopp med bruna kanter, slät och blank. I teet flyter en vit blomma.",
+      width: 500,
+      height: 500,
+    },
   },
   {
     name: "kornblå",
     price: 249,
     category: "med fat",
     rating: 4.5,
+    amount: 0,
+    img: {
+      src: "assets/imgs/16KORNBLÅpexels-trina-snow-1033137.jpg",
+      alt: "En kornblåp högblank kopp fylld med te med mjölk i , på tillhörande fat.",
+      width: 500,
+      height: 500,
+    },
   },
 ];
-console.log(teacups);
+
+const teacupsContainer = document.querySelector("#img-container");
+
+for (i = 0; i < teacups.length; i++) {
+  teacupsContainer.innerHTML += i; // Det funkar att printa i, alltså funkar loopen. Varför kan jag inte loopa varorna?
+}
+
 /**
  *  PLUS-/MINUSKNAPPAR
  */
